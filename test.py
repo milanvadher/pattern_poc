5c8c8e3b873781c94a1cfbe2b170bfbf4ab1879a
Deep Parmar <deep.parmar@yugtia.com>
2018-09-21 10:01:30 +0530

    add volume data in getData route

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson.json_util import dumps
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 client = MongoClient('localhost', 27017)  # localDB
 
 # connect to db
@@ -13,30 +15,69 @@ db = client.pattern_poc
 testTable = db.test
 dataTable = db.dummyData
 
+
+# test api
 @app.route('/test')
 def get_apitest():
     return jsonify({'msg': 'Api is working'})
 
+
+# test db api
 @app.route('/dbtest')
 def get_dbtest():
     return dumps(testTable.find({}))
 
+
+# upload bulk data
 @app.route('/uploadCSV', methods=['POST'])
 def upload_csv():
+    # delete all data
+    dataTable.delete_many({})
 
     # Create variable for uploaded file
     df = pd.read_csv(request.files['fileupload'])
-    
-    records_ = df.to_dict(orient = 'records')
-    dataTable.insert_many(records_ )
+
+    # conver data to dict
+    records_ = df.to_dict(orient='records')
+
+    # upload data to db
+    dataTable.insert_many(records_)
 
     #do something list of dictionaries
-    return jsonify({'success': 'File uploaded successfully!!'})
+    return jsonify({'success': 'Data uploaded successfully!!'})
 
 
+# get Forex (Uploaded data)
 @app.route('/getData')
 def get_dummyData():
-    return dumps(dataTable.find({}))
+    _date = []
+    _open = []
+    _high = []
+    _low =[]
+    _close = []
+    _volume = []
+    for data in dataTable.find({}):
+        _date.append(data['Time (UTC)'])
+        _open.append(data['Open'])
+        _high.append(data['High'])
+        _low.append(data['Low'])
+        _close.append(data['Close'])
+        _volume.append(data['Volume '])
+    
+    return jsonify(
+        {'date': _date},
+        {'open': _open},
+        {'high': _high},
+        {'low': _low},
+        {'close': _close},
+        {'volume': _volume},
+    )
+    # return dumps(dataTable.find({}))
 
+
+# @app.route('/')
+# def generate_chart():
+
+# run api
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
